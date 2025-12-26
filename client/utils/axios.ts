a1410253542002521dd6d0d13be79a09ccf60dbc
urvip <urvip@solguruz.com>
2025-12-26 14:22:57 +0530

    Remove dynamic API base URL and revert to static base URL in axios instance

--- client/utils/axios.ts
+++ client/utils/axios.ts
@@ -1,10 +1,7 @@
 import axios, { AxiosInstance, AxiosError } from "axios";
 
-const API_BASE_URL =
-  import.meta.env.VITE_API_BASE_URL || "http://localhost:8080";
-
 const axiosInstance: AxiosInstance = axios.create({
-  baseURL: `${API_BASE_URL}/api`,
+  baseURL: "/api",
   timeout: 10000,
   headers: {
     "Content-Type": "application/json",
